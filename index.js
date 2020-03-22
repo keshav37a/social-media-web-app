17ffd8d368f8cccd02ceb37d7c4f70cb3d07ba0b
Keshav Mathur <keshav37a@gmail.com>
2020-03-22 19:40:30 +0530

    add static files for individual layouts

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const port = 8000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
 app.use(expressLayouts);
 
 //use express router
 app.use('/', require('./routes/index'));
+
+//extract styles and scripts from sub pages and use it on the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //set ejs in view engine
 app.set('view engine', 'ejs');
@@ -18,7 +23,6 @@ app.listen(port, function(err){
         return;
     }
     console.log(`The server is up and running on port: ${port}`);
-    
 })
 
 //If no file found error then check current directory or check require modules
