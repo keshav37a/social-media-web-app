308add7c119b540fae94db85e11860d7880b7f59
Keshav Mathur <keshav37a@gmail.com>
2020-03-27 14:34:20 +0530

    add checks in login and profile pages. add logout in profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,11 +2,45 @@ const db = require('../config/mongoose');
 //import our model class for db operations
 const User = require('../models/user');
 
+
+
 // For rendering User Profile
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'UserName Profile'
-    });
+    // delete req.cookies['user_id'];
+    let userId = req.cookies['user_id'];
+    console.log('req profile cookie:', userId);
+    if(userId){
+        let userInfo = {};
+        User.findById(userId, function(err, user){
+            if(err){
+                console.log('Error ocuured while looking for id in users_controller>profile');
+                console.log('log line 16');
+                return;
+            }
+            if(user){
+                userInfo['name'] = user.name;
+                userInfo['email'] = user.email;
+                userInfo['CreatedAt'] = user.createdAt;
+                userInfo['UpdatedAt'] = user.updatedAt;
+                console.log('log line 25');
+                return res.render('user_profile', {
+                    title:'User Profile', 
+                    userInfo: userInfo
+                });
+            }
+            else{
+                console.log('log line 32');
+                return res.redirect('/users/login');
+            }
+        })
+        // console.log('UNDEFINED: Redirecting to login page');
+        // return res.redirect('/users/login');
+    }
+    else{
+        console.log('log line 40');
+        return res.redirect('/users/login');
+    }
+    
 }
 
 // For rendering User Settings
@@ -18,9 +52,32 @@ module.exports.settings = function(req, res){
 
 //For rendering login page
 module.exports.login = function(req, res){
-    return res.render('login', {
-        title: 'Login Page'
-    });
+    let userId = req.cookies.user_id;
+    if(userId){
+        User.findById(userId, function(err, user){
+            if(err){
+                console.log('Error finding user in db');
+                return;
+            }
+            if(user){
+                let userInfo = {};
+                userInfo['name'] = user.name;
+                userInfo['email'] = user.email;
+                userInfo['CreatedAt'] = user.createdAt;
+                userInfo['UpdatedAt'] = user.updatedAt;
+                console.log('log line 72');
+                return res.render('user_profile', {
+                    title:'User Profile', 
+                    userInfo: userInfo
+                });
+            }
+        })
+    }
+    else{
+        console.log('log line 80');
+        res.render('login', {title: 'Login Page'});
+    }
+    
 }
 
 // For rendering signup page
@@ -93,12 +150,15 @@ module.exports.loginForm = function(req, res){
                 console.log('Sign In Successful');
                 res.cookie('user_id', user._id);
                 console.log('cookies ', req.cookies);
+                let userInfo = {};
+                userInfo['name'] = user.name;
+                userInfo['email'] = user.email;
+                userInfo['createdAt'] = user.createdAt;
+                userInfo['updatedAt'] = user.updatedAt;
+
                 return res.render('user_profile', {
                     title:'User/Profile', 
-                    name: user.name, 
-                    email: user.email, 
-                    createdAt: user.createdAt,
-                    updatedAt: user.updatedAt
+                    userInfo: userInfo
                 });
             }
 
@@ -116,3 +176,17 @@ module.exports.loginForm = function(req, res){
         }
     })
 }
+
+//For logout
+module.exports.logout = function(req, res){
+    console.log('in logout: ', req.cookies);
+    delete req.cookies['user_id'];
+    console.log('after delete: ', req.cookies);;
+    return res.redirect('/users/login');
+    
+}
+
+
+//silly mistake - used ',' instead of ':' for key value pairs in res.render
+
+//error:Cannot set headers after they are sent to the client - use proper if else otherwise both times render/redirect would be called and you would get the error. async could also be used to counter the problem
--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,6 @@ router.get('/profile', userController.profile);
 router.get('/settings', userController.settings);
 router.get('/signup', userController.signup);
 router.get('/login', userController.login);
+router.get('/logout', userController.logout);
 router.post('/create-session', userController.loginForm);
 router.post('/create-user', userController.signupForm);
