bb160538794ae7ee5fb848c0f9b03d19ed206eca
Keshav Mathur <keshav37a@gmail.com>
2020-04-02 11:31:47 +0530

    code optimize - add async await in comments controller

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,47 +2,38 @@ const Post = require('../models/post');
 const Comment = require('../models/comment');
 var mongoose = require('mongoose');
 
-module.exports.postComment = function(req, res){
+module.exports.postComment = async function(req, res){
     
     console.log('postComment called');
     let postId = req.query.post;
     let userId = req.query.user;
     let content = req.body.content;
-    // console.log(`req.query for post comment: ${postId}`);
-    // console.log(`req.query for user id: ${userId}`);
-    // console.log(`req.query for user id: ${content}`);
+    console.log(`req: post comment: ${postId} userId: ${userId} content: ${content}`);
 
-    Post.findById(postId, function(err, post){
-        if(err){
-            console.log(`error while looking for post in comments_controller: ${err}`);
-            return;
-        }
-        if(post){
-            Comment.create({
-                content: content,
-                user: userId,
-                post: postId
-            }, function(err, comment){
-                if(err){
-                    console.log(`error adding comment to comments table in db in the comments_controller: ${err}`);
-                    return;
-                }
-                else{
-                    post.comments.push(comment);
-                    post.save();
-                    return res.redirect('/');
-                }
-                
-            })
-        }
-        else{
-            return res.redirect('/');
-        }
+    let foundPost = await Post.findById(postId);
+    //if post is found
+    if(foundPost){
+        console.log(`foundPost: ${foundPost}`);
 
-    })
+        try {
+            let createdComment = await Comment.create({content: content,
+                user: userId, 
+                post: postId});
+            console.log(`createdComment: ${createdComment}`);
+            foundPost.comments.push(createdComment);
+            await foundPost.save();    
+        } 
+        catch (err) {
+            console.log(`${err}`);
+        }
+    }
+    else{
+        console.log(`Post not found`);
+    }
+    return res.redirect('/');
 }
 
-module.exports.deleteComment = function(req, res){
+module.exports.deleteComment = async function(req, res){
     console.log('deleteComment in comments_controller called');
     console.log(req.query);
     let loggedInUserId = req.user._id;
@@ -54,34 +45,19 @@ module.exports.deleteComment = function(req, res){
 
     if(loggedInUserId.toString() == userId){
         console.log('authorized to delete');
-        
-        Post.findByIdAndUpdate(postId, {$pull: {comments: commentId}}, function(err, post){
-            if(err){
-                console.log(`${err}`);
-            }
-        })
 
-        Comment.findByIdAndDelete(commentId, function(err, comment){
-            if(err){
-                console.log(`${err}`);
-                return;
-            }
-        })    
-        return res.redirect('/');
-        // Post.findById(postId, function(err, post){
-        //     if(err){
-        //         console.log(`${err}`);
-        //         return;
-        //     }
-        //     if(post){
-        //         for(let i of post.comments._id){
-                    
-        //         }
-        //     }
-        // })
+        try {
+            let updatedPostPromise = Post.findByIdAndUpdate(postId, {$pull: {comments: commentId}});
+            let deletedCommentPromise = Comment.findByIdAndDelete(commentId);
+            const updatedPost = await updatedPostPromise;
+            const deletedComment = await deletedCommentPromise;
+        } 
+        catch (error) {
+            console.log(`${error}`);
+        }
     }
     else{
         console.log('not authorized to delete');
-        return res.redirect('/');
     }
+    return res.redirect('/');
 }
