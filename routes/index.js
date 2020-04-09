6d988665b50f740588c8d7d5c13b7a832eb141b8
Keshav Mathur <keshav37a@gmail.com>
2020-04-09 11:25:41 +0530

    add folder structure for api

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,7 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comment'));
 
+//for api
+router.use('/api', require('./api'));
+
 module.exports = router;
