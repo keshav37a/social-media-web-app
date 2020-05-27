4f747568e7fe504e3815fde3ef9f14d13f788c54
Keshav Mathur <keshav37a@gmail.com>
2020-05-28 02:57:35 +0530

    sending and recieving messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -11,8 +11,50 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self = this;
+
         this.socket.on('connect', function(){
             console.log('connection established using sockets');
         })
+
+        self.socket.emit('join_room', {
+            user_email: this.userEmail,
+            chatroom: 'codeial'
+        })
+
+        self.socket.on('user_joined', function(data){
+            console.log('a user joined ', data);
+        })
+
+        $('#send-message-btn').click((event)=>{
+            event.preventDefault();
+            let msg = $('#send-message-input-text').val();
+            if(msg!=''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    userEmail: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        })
+
+        self.socket.on('recieve_message', function(data){
+            console.log('message recieved: ', data.message);
+            let email = data.userEmail;
+            let className = 'rec-message-div';
+            if(self.userEmail == email)
+                className = 'sent-message-div';
+
+            let newMessage = $('<span>');
+            newMessage.append(data.message);
+            newMessage.append('</span>');
+            newMessage.addClass(className);
+
+            let messageList = $('#message-list');
+            messageList.append(newMessage);
+
+            console.log(messageList);
+        })
     }
 }
+
