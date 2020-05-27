27b0dd828b4538fecada3c2715e30ca17a2355a9
Keshav Mathur <keshav37a@gmail.com>
2020-05-27 14:20:36 +0530

    separate friends list and all users list in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,10 @@
 const Posts = require('../models/post');
 const User = require('../models/user');
+const Friendship = require('../models/friendship');
 const moment = require('moment');
 
 module.exports.home = async function(req, res){
     console.log('home-controller.home');
-    // return res.end('<h1>Express is up for coding. Home Controller called</h1>');
-    // console.log('req-cookies:', req.cookies);
-    // res.cookie('name', 'hehehe');
-    // console.log('res-cookies:', res.cookies);
-
     try{
         let posts = await Posts.find({}).sort({createdAt:-1})
                 .populate('user')
@@ -18,29 +14,33 @@ module.exports.home = async function(req, res){
                         path: 'user'
                     }
                 });
-
         //Convert mongoose object to normal object otherwise it wont be editable                
-        // let postObjArr = [];
-        // for(let i of posts){
-        //     let postObj = i.toObject();
-        //     let formattedDate = moment(postObj.createdAt).format('hh:mm A, DD MMMM');
-        //     postObj.createdAt = formattedDate;
-        //     let commentObjArr = [];
-        //     for(let j of postObj.comments){
-        //         console.log(j);
-        //         formattedDate = moment(j.createdAt).format('hh:mm A, DD MMMM');
-        //         j.createdAt = formattedDate;
-        //     }
-        //     postObjArr.comments = commentObjArr;
-        //     postObjArr.push(postObj);
-        // }
-
         let users = await User.find({});
-
+        let friendsArr = [];
+        if(req.user){
+            let loggedInUser = await User.findById(req.user._id).populate({path: 'friendships', populate: 'from_user to_user'});
+            console.log('req.user: ', loggedInUser);
+            let friendshipIdArr = loggedInUser.friendships;
+            for(let i=0; i<friendshipIdArr.length; i++){
+                let friendshipObject = friendshipIdArr[i];
+                // console.log('friendshipObject: ', friendshipObject);;
+                let fromUser = friendshipObject.from_user;
+                let toUser = friendshipObject.to_user;
+                if(fromUser._id.toString()==loggedInUser._id.toString()){
+                    friendsArr.push(toUser);
+                }
+                else{
+                    friendsArr.push(fromUser);
+                }
+            }
+            console.log(friendsArr);
+        }
+        
         return res.render('home', {
             title:'Posts', 
             posts: posts,
-            userAll: users
+            userAll: users,
+            friendsArr: friendsArr
         })
     }
     catch(err){
